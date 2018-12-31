411adbe2c58c40e14469a55de39a2412b0205ae5
Xule Zhou <other.marcus@icloud.com>
2018-12-31 16:56:59 -0500

    Add .travis.yml

---
+++ NineAnimatorTests/NineAnimatorTests.swift
@@ -0,0 +1,20 @@
+//
+//  NineAnimatorTests.swift
+//  NineAnimatorTests
+//
+//  Created by Xule Zhou on 12/31/18.
+//  Copyright © 2018 Marcus Zhou. All rights reserved.
+//
+
+import XCTest
+
+class NineAnimatorTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+}
