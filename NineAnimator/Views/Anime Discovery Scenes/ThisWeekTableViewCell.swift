772a120a3422b0d879a9037254291bf5ec2b11d1
Marcus Zhou <other.marcus@icloud.com>
2020-02-08 13:23:28 -0500

    Fix Watch Next screen layout issues

--- NineAnimator/Views/Anime Discovery Scenes/ThisWeekTableViewCell.swift
+++ NineAnimator/Views/Anime Discovery Scenes/ThisWeekTableViewCell.swift
@@ -47,12 +47,12 @@ class ThisWeekTableViewCell: UITableViewCell, UICollectionViewDataSource, UIColl
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let cellParentBounds = collectionView.bounds
-        let inset = collectionView.adjustedContentInset
-        let height = cellParentBounds.height - inset.top - inset.bottom - scrollBarHeightInset
-        let maxWidth = cellParentBounds.width - inset.left - inset.right
-        
-        return CGSize(width: min(maxWidth, maxItemWidth), height: height)
+        var cellSize = collectionView.bounds
+            .inset(by: collectionView.layoutMargins)
+            .size
+//        cellSize.height -= scrollBarHeightInset
+        cellSize.width = min(maxItemWidth, cellSize.width)
+        return cellSize
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
