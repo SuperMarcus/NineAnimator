c037f6f5a0c980d8d44de7ac1f8e84c52cd8a21e
Catta1997 <34333654+Catta1997@users.noreply.github.com>
2020-03-27 18:23:10 +0100

    Indentation fix

--- NineAnimator/Models/Anime Source/animeworld.tv/AnimeWorld+Search.swift
+++ NineAnimator/Models/Anime Source/animeworld.tv/AnimeWorld+Search.swift
@@ -16,89 +16,80 @@
 //  You should have received a copy of the GNU General Public License
 //  along with NineAnimator.  If not, see <http://www.gnu.org/licenses/>.
 //
-
 import Alamofire
 import Foundation
 import SwiftSoup
-
 extension NASourceAnimeWorld {
     class SearchAgent: ContentProvider {
-            var totalPages: Int? { 1 }
-            var availablePages: Int { _results == nil ? 0 : 1 }
-            var moreAvailable: Bool { _results == nil }
-            private let parent: NASourceAnimeWorld
-            private var requestTask: NineAnimatorAsyncTask?
-            private var _results: [AnimeLink]?
-            var title: String
-            weak var delegate: ContentProviderDelegate?
-            
-            func links(on page: Int) -> [AnyLink] {
-                page == 0 ? _results?.map { .anime($0) } ?? [] : []
-            }
-            
-            func more() {
-                guard requestTask == nil && moreAvailable else { return }
-                requestTask = parent.request(
-                    browsePath: "/search",
-                    query: [ "keyword": title ]
-                ) .then {
-                    [weak self] response -> [AnimeLink] in
-                    guard let self = self else { throw NineAnimatorError.unknownError }
-                    let bowl = try SwiftSoup.parse(response)
-                    let entries = try bowl.select("div.film-list>div.item")
-                    let resultingLinks = entries.compactMap {
-                        entry -> AnimeLink? in
-                        do {
-                            let animeTitle = try entry.select("a.name"/*"h6.card-title>b"*/)
-                            let title = try animeTitle.text()
-                            if title.isEmpty { return nil }
-                            let animeLinkPath = try entry.select("div>a")
-                            let url = try animeLinkPath.attr("href")
-                            let animeUrl = try URL(
-                                string: url,
-                                relativeTo: self.parent.endpointURL
-                            ).tryUnwrap()
-                            let animeArtworkPath = try entry.select("img").attr("src")
-                            
-                            guard let coverImage = URL(string: animeArtworkPath)
-                            else {
-                                return nil
-                            }
-                            return AnimeLink(
-                                title: title,
-                                link: animeUrl,
-                                image: coverImage,
-                                source: self.parent
-                            )
-                        } catch { return nil }
-                    }
-                    
-                    guard !resultingLinks.isEmpty else {
-                        throw NineAnimatorError.searchError("No results found")
-                    }
-                    
-                    return resultingLinks
-                } .error {
-                    [weak self] error in
-                    guard let self = self else { return }
-                    self.delegate?.onError(error, from: self)
-                    self.requestTask = nil
-                } .finally {
-                    [weak self] results in
-                    guard let self = self else { return }
-                    self._results = results
-                    self.delegate?.pageIncoming(0, from: self)
-                    self.requestTask = nil
+    var totalPages: Int? { 1 }
+    var availablePages: Int { _results == nil ? 0 : 1 }
+    var moreAvailable: Bool { _results == nil }
+    private let parent: NASourceAnimeWorld
+    private var requestTask: NineAnimatorAsyncTask?
+    private var _results: [AnimeLink]?
+    var title: String
+    weak var delegate: ContentProviderDelegate?
+    func links(on page: Int) -> [AnyLink] {
+        page == 0 ? _results?.map { .anime($0) } ?? [] : []
+        }
+        func more() {
+            guard requestTask == nil && moreAvailable else { return }
+            requestTask = parent.request(
+                browsePath: "/search",
+                query: [ "keyword": title ]
+            ) .then {
+                [weak self] response -> [AnimeLink] in
+                guard let self = self else { throw NineAnimatorError.unknownError }
+                let bowl = try SwiftSoup.parse(response)
+                let entries = try bowl.select("div.film-list>div.item")
+                let resultingLinks = entries.compactMap {
+                    entry -> AnimeLink? in
+                    do {
+                        let animeTitle = try entry.select("a.name"/*"h6.card-title>b"*/)
+                        let title = try animeTitle.text()
+                        if title.isEmpty { return nil }
+                        let animeLinkPath = try entry.select("div>a")
+                        let url = try animeLinkPath.attr("href")
+                        let animeUrl = try URL(
+                            string: url,
+                            relativeTo: self.parent.endpointURL
+                        ).tryUnwrap()
+                        let animeArtworkPath = try entry.select("img").attr("src")
+                        guard let coverImage = URL(string: animeArtworkPath)
+                        else {
+                            return nil
+                        }
+                        return AnimeLink(
+                            title: title,
+                            link: animeUrl,
+                            image: coverImage,
+                            source: self.parent
+                        )
+                    } catch { return nil }
                 }
-            }
-            
-            init(query: String, parent: NASourceAnimeWorld) {
-                self.title = query
-                self.parent = parent
+                guard !resultingLinks.isEmpty else {
+                    throw NineAnimatorError.searchError("No results found")
+                }
+                return resultingLinks
+            } .error {
+                [weak self] error in
+                guard let self = self else { return }
+                self.delegate?.onError(error, from: self)
+                self.requestTask = nil
+            } .finally {
+                [weak self] results in
+                guard let self = self else { return }
+                self._results = results
+                self.delegate?.pageIncoming(0, from: self)
+                self.requestTask = nil
             }
         }
-        
-        func search(keyword: String) -> ContentProvider {
-            SearchAgent(query: keyword, parent: self)
+        init(query: String, parent: NASourceAnimeWorld) {
+            self.title = query
+            self.parent = parent
         }
+    }
+    func search(keyword: String) -> ContentProvider {
+        SearchAgent(query: keyword, parent: self)
+    }
 }
