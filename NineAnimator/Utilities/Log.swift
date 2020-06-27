619287b8a269b2824ec987f582b1e4d9e8710b7d
Marcus Zhou <other.marcus@icloud.com>
2020-06-26 22:35:11 -0400

    Update loggings

--- NineAnimator/Utilities/Log.swift
+++ NineAnimator/Utilities/Log.swift
@@ -20,21 +20,19 @@
 import Foundation
 import os
 
-/**
- The logger class that is used to wrap around the os unified logging system
- */
+/// The logger class that is used to wrap around the os unified logging system
 class NineAnimatorLogger {
     typealias Logger = ((StaticString, Any...) -> Void)
     
     private let customLog: OSLog
     
-    //The basic four levels of logging
+    // The basic four levels of logging
     let info: Logger
     let debug: Logger
     let error: Logger
     let fault: Logger
     
-    //Some convenient accesses
+    // Some convenient accesses
     func error(_ error: Error?) {
         if let error = error {
             self.error("%@", error)
@@ -54,9 +52,7 @@ class NineAnimatorLogger {
     }
 }
 
-/**
- The public log object that should be used
- */
+/// The public log object that should be used
 let Log = NineAnimatorLogger()
 
 /*
@@ -80,18 +76,21 @@ let Log = NineAnimatorLogger()
 // We shall start by disabling all lintings
 // swiftlint:disable all
 
-private func generateLogger(_ customLog: OSLog, for level: OSLogType)
-    -> NineAnimatorLogger.Logger {
-        return { [unowned customLog] (message: StaticString, arguments: Any...) in
-            let args = arguments.map { String(describing: $0) }
-            switch args.count { // I'm really curious to know if there is any better way of wrapping os_log
-            case 0: os_log(message, log: customLog, type: level)
-            case 1: os_log(message, log: customLog, type: level, args[0])
-            case 2: os_log(message, log: customLog, type: level, args[0], args[1])
-            case 3: os_log(message, log: customLog, type: level, args[0], args[1], args[2])
-            case 4: os_log(message, log: customLog, type: level, args[0], args[1], args[2], args[3])
-            case 5: os_log(message, log: customLog, type: level, args[0], args[1], args[2], args[3], args[4])
-            default: os_log("Logger function is called with too many arguments", log: customLog, type: .error)
+extension NineAnimatorLogger {
+    private func generateLogger(_ customLog: OSLog, for level: OSLogType)
+        -> NineAnimatorLogger.Logger {
+            return { [unowned customLog] (message: StaticString, arguments: Any...) in
+                let args = arguments.map { String(describing: $0) }
+                switch args.count { // I'm really curious to know if there is any better way of wrapping os_log
+                case 0: os_log(message, log: customLog, type: level)
+                case 1: os_log(message, log: customLog, type: level, args[0])
+                case 2: os_log(message, log: customLog, type: level, args[0], args[1])
+                case 3: os_log(message, log: customLog, type: level, args[0], args[1], args[2])
+                case 4: os_log(message, log: customLog, type: level, args[0], args[1], args[2], args[3])
+                case 5: os_log(message, log: customLog, type: level, args[0], args[1], args[2], args[3], args[4])
+                default:
+                    os_log("[NineAnimatorLogger] Logger function is called with too many arguments.", log: customLog, type: .error)
+                }
             }
-        }
+    }
 }
