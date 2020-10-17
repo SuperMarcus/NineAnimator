1f6aace147c7995d3dc7ae503b778fee2c48412b
Marcus Zhou <other.marcus@icloud.com>
2020-10-17 01:41:39 -0400

    [WIP] Update 9anime anime parsing

--- NineAnimator/Models/Anime Source/9anime.ru/NineAnime+Anime.swift
+++ NineAnimator/Models/Anime Source/9anime.ru/NineAnime+Anime.swift
@@ -21,6 +21,193 @@ import Foundation
 
 extension NASourceNineAnime {
     func anime(from link: AnimeLink) -> NineAnimatorPromise<Anime> {
-        .fail()
+        self.requestDescriptor().thenPromise {
+            _ in self.animeInfo(in: link.link)
+        } .thenPromise {
+            animePageInfo in
+            self.availableServers(
+                of: animePageInfo.siteId,
+                refererLink: animePageInfo.animeLink.link
+            ) .then { (animePageInfo, $0) }
+        } .then {
+            animePageInfo, serverList in
+            nil
+        }
     }
+    
+    /// Retrieve anime info from the anime page
+    func animeInfo(in pageUrl: URL) -> NineAnimatorPromise<AnimeInfo> {
+        self.requestManager.request(
+            url: pageUrl,
+            handling: .browsing
+        ) .responseBowl
+          .then {
+            bowl in
+            // Basic Anime Info
+            let infoContainer = try bowl.select("#info")
+            
+            let animeArtworkString = try infoContainer.select("img").attr("src")
+            let animeArtworkUrl = URL(
+                protocolRelativeString: animeArtworkString,
+                relativeTo: pageUrl
+            ) ?? NineAnimator.placeholderArtworkUrl
+            
+            let animeTitle = try infoContainer
+                .select(".title")
+                .text()
+                .trimmingCharacters(in: .whitespacesAndNewlines)
+            let animeAlias = try infoContainer
+                .select(".alias")
+                .text()
+                .trimmingCharacters(in: .whitespacesAndNewlines)
+            let animeSynopsis = try infoContainer
+                .select(".shorting")
+                .text()
+                .trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            let animeLink = AnimeLink(
+                title: animeTitle,
+                link: pageUrl,
+                image: animeArtworkUrl,
+                source: self
+            )
+            
+            // Anime Identifier
+            let playerContainer = try bowl.select(".watchpage")
+            let animeId = try playerContainer.attr("data-id")
+            
+            // Metadata
+            let animePageAttributes = try bowl.select(".meta>*>div").reduce(into: [String: String]()) {
+                dict, currentElement in
+                do {
+                    if let metaValueSpan = try currentElement.select("span").first() {
+                        // Get value then key
+                        let metaValue = try metaValueSpan.text().trimmingCharacters(in: .whitespacesAndNewlines)
+                        try metaValueSpan.remove()
+                        var metaKey = try currentElement.text().trimmingCharacters(in: .whitespacesAndNewlines)
+                        
+                        if metaKey.hasSuffix(":") {
+                            metaKey.removeLast()
+                            metaKey = metaKey.trimmingCharacters(in: .whitespacesAndNewlines)
+                        }
+                        
+                        dict[metaKey] = metaValue
+                    }
+                } catch {
+                    Log.error("[NASourceNineAnime] Unable to parse a metadata (%@) because of an error: %@", currentElement, error)
+                }
+            }
+            
+            return AnimeInfo(
+                animeLink: animeLink,
+                alias: animeAlias,
+                synopsis: animeSynopsis,
+                siteId: animeId,
+                attributes: animePageAttributes
+            )
+        }
+    }
+    
+    /// Retrieve the available servers and episodes of an anime
+    func availableServers(of animeId: String, refererLink: URL) -> NineAnimatorPromise<AnimeServerList> {
+        self.requestManager.request(
+            "ajax/anime/servers",
+            handling: .ajax,
+            parameters: [
+                "id": animeId,
+                "ep": "",
+                "episode": ""
+            ],
+            headers: [
+                "Accept": "text/html, */*; q=0.01",
+                "Referer": refererLink.absoluteString
+            ]
+        ) .responseBowl
+          .then {
+            bowl in
+            let serverList = try bowl
+                .select(".servers>span")
+                .map {
+                    serverElement in AnimeServer(
+                        id: try serverElement.attr("data-id"),
+                        elementId: try serverElement.attr("id"),
+                        name: try serverElement.text().trimmingCharacters(in: .whitespacesAndNewlines)
+                    )
+                }
+            
+            let episodeList = try bowl
+                .select(".episodes a")
+                .reduce(into: [EpisodeInfo]()) {
+                    list, episodeElement in
+                    // Read and decode the sources attribute
+                    let sourcesAttributeValue = try episodeElement.attr("data-sources")
+                    guard let sourcesAttributeData = sourcesAttributeValue.data(using: .utf8),
+                       let decodedSources = try JSONSerialization.jsonObject(
+                        with: sourcesAttributeData,
+                        options: []
+                       ) as? [StreamServerID: EpisodeResourceID] else {
+                        return
+                    }
+                    
+                    // Other episode info
+                    let episodePageLink = try URL(
+                        protocolRelativeString: try episodeElement.attr("href"),
+                        relativeTo: refererLink
+                    ).tryUnwrap()
+                    
+                    let episodeName = try episodeElement
+                        .text()
+                        .trimmingCharacters(in: .whitespacesAndNewlines)
+                    let episodeNameNormalized = try episodeElement
+                        .attr("data-name-normalized")
+                        .trimmingCharacters(in: .whitespacesAndNewlines)
+                    
+                    let episodeInfo = EpisodeInfo(
+                        name: episodeName,
+                        normalizedName: episodeNameNormalized,
+                        link: episodePageLink,
+                        resourceMap: decodedSources
+                    )
+                    
+                    list.append(episodeInfo)
+                }
+            
+            return AnimeServerList(
+                servers: serverList,
+                episodes: episodeList
+            )
+        }
+    }
+}
+
+// MARK: - Data Structs
+extension NASourceNineAnime {
+    struct AnimeInfo {
+        var animeLink: AnimeLink
+        var alias: String
+        var synopsis: String
+        var siteId: String
+        var attributes: [String: String]
+    }
+    
+    struct AnimeServerList {
+        var servers: [AnimeServer]
+        var episodes: [EpisodeInfo]
+    }
+    
+    struct AnimeServer: Hashable {
+        var id: StreamServerID
+        var elementId: String
+        var name: String
+    }
+    
+    struct EpisodeInfo {
+        var name: String
+        var normalizedName: String
+        var link: URL
+        var resourceMap: [StreamServerID: EpisodeResourceID]
+    }
+    
+    typealias StreamServerID = String
+    typealias EpisodeResourceID = StreamServerID
 }
