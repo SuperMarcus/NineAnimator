c6bfec3ebbf72ed25ef1c51c18528d3fadb827d5
Tommy <52971804+ISnackable@users.noreply.github.com>
2022-03-22 18:05:10 +0800

    Fix StreamSB Parser

--- Modules/Sources/NineAnimatorNativeParsers/MediaParsers/StreamSBParser.swift
+++ Modules/Sources/NineAnimatorNativeParsers/MediaParsers/StreamSBParser.swift
@@ -45,55 +45,115 @@ class StreamSBParser: VideoProviderParser {
         let url: String
     }
     
-    private let apiPath: String = "https://watchsb.com/sourcesx38/7361696b6f757c7c\("HEXVIDEOID")7c7c7361696b6f757c7c73747265616d7362/7361696b6f757c7c363136653639366436343663363136653639366436343663376337633631366536393664363436633631366536393664363436633763376336313665363936643634366336313665363936643634366337633763373337343732363536313664373336327c7c7361696b6f757c7c73747265616d7362"
+    private static let versionRegex = try! NSRegularExpression(
+        pattern: #"/app\.v1\.(\d{2})\.js"#,
+        options: .caseInsensitive
+    )
+    
+    private static let part2Regex = try! NSRegularExpression(
+        pattern: #"\'(ces\w{2,3})\'"#,
+        options: .caseInsensitive
+    )
+    
+    private static let part3Regex = try! NSRegularExpression(
+        pattern: #"\'(\d{1,2}\/)\'"#,
+        options: .caseInsensitive
+    )
+    
+    private let apiPath: String = "https://watchsb.com/\("SOURCES")/566d337678566f743674494a7c7c\("HEXVIDEOID")7c7c346b6767586d6934774855537c7c73747265616d7362/6565417268755339773461447c7c346133383438333436313335376136323337373433383634376337633465366534393338373136643732373736343735373237613763376334363733353737303533366236333463353333363534366137633763373337343732363536313664373336327c7c6b586c3163614468645a47617c7c73747265616d7362"
+    
+    private func getAlias(url: URL, with session: Session) -> NineAnimatorPromise<String> {
+        NineAnimatorPromise {
+            callback in session.request(url).responseString {
+                callback($0.value, $0.error)
+            }
+        } .thenPromise {
+            responseContent in
+            
+            // let versionNo = try (
+            //     StreamSBParser.versionRegex.firstMatch(in: responseContent)?.firstMatchingGroup
+            // ).tryUnwrap()
+            let jsURL = try (
+                StreamSBParser.versionRegex.firstMatch(in: responseContent)?[safe: 0]
+            ).tryUnwrap()
+            
+            return NineAnimatorPromise {
+                callback in session.request("https://watchsb.com/js/\(jsURL)", headers: [
+                    "watchsb": "streamsb",
+                    "Referer": url.absoluteString
+                ]).responseString {
+                    callback($0.value, $0.error)
+                }
+            } .then {
+                resourceInfoRes in
+                
+                let part1 = "sour"
+                let part2 = try (
+                    StreamSBParser.part2Regex.firstMatch(in: resourceInfoRes)?.firstMatchingGroup
+                ).tryUnwrap()
+                
+                let part3Match = StreamSBParser.part3Regex.firstMatch(in: resourceInfoRes)
+                var part3 = ""
+                if let part3Match = part3Match {
+                    part3 = try (part3Match.firstMatchingGroup).tryUnwrap()
+                }
+                let alias = part1 + part2 + part3
+                
+                return alias
+            }
+        }
+    }
     
     func parse(episode: Episode, with session: Session, forPurpose _: Purpose, onCompletion handler: @escaping NineAnimatorCallback<PlaybackMedia>) -> NineAnimatorAsyncTask {
-        NineAnimatorPromise<PlaybackMedia> {
-            callback in
-
-            let videoID = episode.target.deletingPathExtension().lastPathComponent
-            guard episode.target.pathComponents.count == 3 else {
-                handler(nil, NineAnimatorError.urlError)
-                return nil
-            }
-            
-            let hexVideoID = Data(videoID.utf8).map {
-                String(format: "%x", $0 )
-            }.joined()
-            let sourceURLString = self.apiPath.replacingOccurrences(of: "HEXVIDEOID", with: hexVideoID)
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            
-            // Make the request to the URL
-            return session.request(
-                sourceURLString,
-                headers: [
-                    "watchsb": "streamsb",
-                    "Referer": episode.target.absoluteString
-                ]
-            ) .responseDecodable(of: StreamSBAPIResponse.self, decoder: decoder) {
-                response in
-                switch response.result {
-                case .success(let decodedResponse):
-                    do {
-                        let streamData = decodedResponse.streamData
-
-                        let resourceUrl = try URL(string: streamData.file ?? streamData.backup).tryUnwrap(.urlError)
-
-                        Log.info("(StreamSB Parser) found asset at %@", resourceUrl.absoluteString)
-
-                        callback(BasicPlaybackMedia(
-                            url: resourceUrl,
-                            parent: episode,
-                            contentType: "application/vnd.apple.mpegurl",
-                            headers: [:],
-                            isAggregated: true
-                        ), nil)
-                    } catch { callback(nil, error) }
-                default: callback(nil, response.error ?? NineAnimatorError.unknownError)
+        // thanks @awsomedude
+        self.getAlias(url: episode.target, with: session)
+            .thenPromise {
+                alias -> NineAnimatorPromise<PlaybackMedia> in
+                let videoID = episode.target.deletingPathExtension().lastPathComponent
+                
+                let hexVideoID = Data(videoID.utf8).map {
+                    String(format: "%x", $0 )
+                }.joined()
+                
+                var sourceURLString = self.apiPath.replacingOccurrences(of: "HEXVIDEOID", with: hexVideoID)
+                sourceURLString = sourceURLString.replacingOccurrences(of: "SOURCES", with: alias)
+                
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                
+                return NineAnimatorPromise {
+                    callback in session.request(
+                        sourceURLString,
+                        headers: [
+                            "watchsb": "streamsb",
+                            "referer": episode.target.absoluteString,
+                            "user-agent": self.defaultUserAgent
+                        ]
+                    ) .responseData {
+                        callback($0.value, $0.error)
+                    }
+                } .then {
+                    try decoder.decode(StreamSBAPIResponse.self, from: $0)
+                } .then {
+                    decoded in
+                    
+                    let streamData = decoded.streamData
+                    
+                    let resourceUrl = try URL(string: streamData.file ?? streamData.backup).tryUnwrap(.urlError)
+                    
+                    Log.info("(StreamSB Parser) found asset at %@", resourceUrl.absoluteString)
+                    
+                    return BasicPlaybackMedia(
+                        url: resourceUrl,
+                        parent: episode,
+                        contentType: "application/vnd.apple.mpegurl",
+                        headers: [
+                            "user-agent": self.defaultUserAgent
+                        ],
+                        isAggregated: true
+                    )
                 }
-            }
-        } .handle(handler)
+            }.handle(handler)
     }
     
     func isParserRecommended(forPurpose purpose: Purpose) -> Bool {
